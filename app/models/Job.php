4d6bc6119e5f53aef02f01f3c0542c431ec42ac4
Sachin <sachin@systematixtechnocrates.com>
2012-03-20 16:58:47 +0530

    add pagination function in model

--- app/models/Job.php
+++ app/models/Job.php
@@ -3,6 +3,22 @@
 class Job extends AppModel {
     var $name = 'Job';
     var $useTable = 'jobs';
+
+public function paginateCount($conditions = null, $recursive = 0, $extra = array())
+{
+    if ( isset($extra['group']) )
+    {
+        $parameters = compact('conditions', 'recursive');
+        $count = $this->find('count', $parameters);
+    }
+    else
+    {
+        $parameters = compact('conditions', 'recursive');
+        $count = $this->find('count', array_merge($parameters, $extra));
+    }
+    return $count;
+}
+
 }
 
 ?>
