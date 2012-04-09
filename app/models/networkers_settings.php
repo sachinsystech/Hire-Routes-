07381ec81eb091ce3870f13a57e666e720f47105
sachin <sachin@systematixtechnocrates.com>
2012-04-09 14:59:31 +0530

    Rename file networkers_setting to networker_settings

--- app/models/networkers_settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class NetworkerSettings extends AppModel {
-    var $name = 'NetworkerSettings ';
-    var $useTable = 'networker_settings';
-    //var $order = "NetworkerSettings.industry ASC";
-}
-
-?>
