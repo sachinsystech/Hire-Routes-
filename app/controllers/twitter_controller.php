06ecc31b425ea9e44686809bb34060afa35cec79
sachin <sachin@systematixtechnocrates.com>
2012-10-22 14:05:31 +0530

    changes for twitter messages

--- app/controllers/twitter_controller.php
+++ app/controllers/twitter_controller.php
@@ -131,7 +131,7 @@ class TwitterController extends AppController {
                 	}else{
                 		$invitationUrl = Configure::read('httpRootURL').'?intermediateCode='.$intermediateCode."&icc=".$icc;	
                 	}
-                	$message = $this->params['form']['message']." Connect with us &#62;&#62; ".$invitationUrl;
+                	$message = $this->params['form']['message']." Connect with us  ".$invitationUrl;
                 	/*
                     $result = $twitterObj->post_direct_messagesNew( array('user' => $fbuser->id, 'text' => $message));
                     $resp = $result->response;*/
